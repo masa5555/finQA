ba7bee14745e768df4c0939c4a43cbd3c2afd21a
masa5555 <masa57887@gmail.com>
2020-09-30 16:16:58 +0900

    登録機能を追加　SQLがうまく行かない修正1

--- regist.php
+++ regist.php
@@ -1,40 +1,59 @@
 <?php
-    header("Content-Type: text/html;charset=utf-8");
 
-    $id = htmlspecialchars($_POST["id"]);
-    $pw = htmlspecialchars($_POST["password"]);
+header("Content-Type: text/html;charset=utf-8");
 
-    $dest = "./index.php";
+$id = htmlspecialchars($_POST["id"]);
+$pw = htmlspecialchars($_POST["password"]);
 
-    //制限する長さに変える
-    if(strcmp($id, "") == 0 || strcmp($pw, "" ) == 0){
-        exit("エラー:　IDまたはPWが空白です。");
-    }
+$dest = "./index.php";
 
-    include "heroku_pg_connect.php";
+//制限する長さに変える
+if(strcmp($id, "") == 0 || strcmp($pw, "" ) == 0){
+    exit("エラー:　IDまたはPWが空白です。");
+}
 
-    $confirm_sql = "SELECT name FROM users WHERE :id == name;";
-    $query = $pdo->prepare($confirm_sql);
+//include "heroku_pg_connect.php";
+
+try {
+    $db = parse_url(getenv("DATABASE_URL"));
+
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+
+}catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+$confirm_sql = "SELECT name FROM users WHERE :id == name;";
+$query = $pdo->prepare($confirm_sql);
+$query->bindParam(':id', $id);
+$query->execute();
+$result = $query->fetch();
+
+if(pg_num_rows($result)){
+    exit("既に登録されています");
+}else{
+    $insert_sql = 
+        "INSERT INTO users (name, password, created_at, updated_at) 
+        VALUES (':id', ':hash_pw', ':timestamp', ':timestamp');";
+    $query = $pdo->prepare($insert_sql);
+
     $query->bindParam(':id', $id);
+    $query->bindParam(':hash_pw', password_hash($pw, PASSWORD_DEFAULT));
+    $query->bindParam('timestamp', date('Y-m-d H:i:s', time()));
+    
     $query->execute();
-    $result = $query->fetch();
 
-    if(pg_num_rows($result)){
-        exit("既に登録されています");
-    }else{
-        $insert_sql = 
-            "INSERT INTO users (name, password, created_at, updated_at) 
-            VALUES (':id', ':hash_pw', ':timestamp', ':timestamp');";
-        $query = $pdo->prepare($insert_sql);
+    header("Location: $dest");
+}
 
-        $query->bindParam(':id', $id);
-        $query->bindParam(':hash_pw', password_hash($pw, PASSWORD_DEFAULT));
-        $query->bindParam('timestamp', date('Y-m-d H:i:s', time()));
-        
-        $query->execute();
+unset($pdo);
 
-        header("Location: $dest");
-    }
 
-    unset($pdo);
 ?>
