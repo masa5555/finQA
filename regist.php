50f4d0cd7434ed02987fa8886fbacbe0a6e7bbf1
masa5555 <masa57887@gmail.com>
2020-10-01 21:17:32 +0900

    登録機能　SQL動作確認

--- regist.php
+++ regist.php
@@ -3,14 +3,14 @@
 header("Content-Type: text/html;charset=utf-8");
 
 //変数の名前をリファクタする
-$id = htmlspecialchars($_POST["id"]);
+$post_id = htmlspecialchars($_POST["id"]);
 $pw = htmlspecialchars($_POST["password"]);
 
 $dest = "./index.php";
 
 //制限する長さに変える
-if(strcmp($id, "") == 0 || strcmp($pw, "" ) == 0){
-    exit("エラー:　IDまたはPWが空白です。");
+if(strcmp($post_id, "") == 0 || strcmp($pw, "" ) == 0){
+    exit("エラー:　名前またはPWが空白です。");
 }
 
 try {
@@ -29,29 +29,30 @@ try {
     echo $e->getMessage();
 }
 
-$confirm_sql = "SELECT name FROM users WHERE :id = name;";
+$confirm_sql = "SELECT name FROM users WHERE :post_id = name;";
 $query = $pdo->prepare($confirm_sql);
-$query->bindParam(':id', $id);
+$query->bindParam(':post_id', $post_id);
 $query->execute();
-$result = $query->fetch();
+$result = $query->fetch(PDO::FETCH_COLUMN);
 
 if($result){
     exit("既に登録されています");
 }else{
     $insert_sql = 
         "INSERT INTO users (name, password, created_at, updated_at) 
-        VALUES (:id, :hash_pw, :timestamp, :timestamp);";
+        VALUES (:post_id, :hash_pw, :timestamp, :timestamp);";
     $query = $pdo->prepare($insert_sql);
 
     $hash_pw = password_hash($pw, PASSWORD_DEFAULT);
     $timestamp = date('Y-m-d H:i:s', time());
 
-    $query->bindParam(':id', $id);
+    $query->bindParam(':post_id', $post_id);
     $query->bindParam(':hash_pw', $hash_pw);
     $query->bindParam(':timestamp', $timestamp);
     
     $query->execute();
 
+    setcookie("success_regist", 1, time()+5);
     header("Location: $dest");
 }
 
