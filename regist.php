1492a808bc4aa0fccf5e300f196cebec83231765
masa5555 <masa57887@gmail.com>
2020-10-01 18:35:17 +0900

    登録機能を追加　SQL文のtypo修正3

--- regist.php
+++ regist.php
@@ -2,6 +2,7 @@
 
 header("Content-Type: text/html;charset=utf-8");
 
+//変数の名前をリファクタする
 $id = htmlspecialchars($_POST["id"]);
 $pw = htmlspecialchars($_POST["password"]);
 
@@ -11,8 +12,6 @@ $dest = "./index.php";
 if(strcmp($id, "") == 0 || strcmp($pw, "" ) == 0){
     exit("エラー:　IDまたはPWが空白です。");
 }
-
-//include "heroku_pg_connect.php";
 
 try {
     $db = parse_url(getenv("DATABASE_URL"));
@@ -30,6 +29,7 @@ try {
     echo $e->getMessage();
 }
 
+//　同じ名前を入力したときもDB登録されてしまう。fetchがうまく行ってない
 $confirm_sql = "SELECT name FROM users WHERE :id == name;";
 $query = $pdo->prepare($confirm_sql);
 $query->bindParam(':id', $id);
@@ -44,9 +44,12 @@ if($result){
         VALUES (:id, :hash_pw, :timestamp, :timestamp);";
     $query = $pdo->prepare($insert_sql);
 
+    $hash_pw = password_hash($pw, PASSWORD_DEFAULT);
+    $timestamp = date('Y-m-d H:i:s', time());
+    
     $query->bindParam(':id', $id);
-    $query->bindParam(':hash_pw', password_hash($pw, PASSWORD_DEFAULT));
-    $query->bindParam(':timestamp', date('Y-m-d H:i:s', time()));
+    $query->bindParam(':hash_pw', $hash_pw);
+    $query->bindParam(':timestamp', $timestamp);
     
     $query->execute();
 
